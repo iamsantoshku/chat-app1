c7fa00994856c042aa162c3753d9b36df4482e29
iamsantoshku <santoshkr1592000@gmail.com>
2024-05-20 20:22:11 +0530

    first commit

---
+++ public/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SetAvatar from "./components/SetAvatar";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+// import Header from "./components/Header";
+export default function App() {
+  return (
+    
+    <>
+    {/* <Header/> */}
+     <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/setAvatar" element={<SetAvatar />} />
+        <Route path="/" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+    </>
+   
+  );
+}
